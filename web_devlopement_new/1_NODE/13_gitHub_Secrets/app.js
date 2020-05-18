f8c93f60e78635322dd1356186486f63f67e5e03
Ashutosh Kumar <ak2417942@gmail.com>
2020-05-18 23:48:59 +0530

    Level 1 authentication

---
+++ web_devlopement_new/1_NODE/13_gitHub_Secrets/app.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
+
+const app = express();
+
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+//connecting to the database
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser : true , useUnifiedTopology:true});
+
+//creating a schema
+const userSchema = new mongoose.Schema({
+  email : String,
+  password : String
+});
+
+//creating a model
+const User = mongoose.model("User", userSchema);
+
+
+//handling get request
+app.get("/", function(req,res){
+  res.render("home");
+});
+
+app.get("/login", function(req,res){
+  res.render("login");
+});
+
+app.get("/register", function(req,res){
+  res.render("register");
+});
+
+//handling all the post request
+app.post("/register", function(req,res){
+
+   const newUser = new User({
+     email :req.body.username,
+     password : req.body.password
+   });
+
+   newUser.save(function(err){
+      if(err){
+        console.log(err);
+      } else {
+        res.render("secrets");
+      }
+   });
+});
+
+app.post("/login", function(req,res){
+
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne( {email : username}, function(err,foundUser){
+       if(err){
+         console.log(err);
+       } else {
+          if(foundUser) {
+            if(foundUser.password === password)
+            res.render("secrets");
+          }
+       }
+    });
+});
+
+
+app.listen(3000, function(){
+  console.log("Server Started on port 3000");
+});
