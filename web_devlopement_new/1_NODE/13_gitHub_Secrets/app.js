d6110ac0e19c7b626a30edaac3e344a76b074232
Ashutosh Kumar <ak2417942@gmail.com>
2020-05-20 13:16:41 +0530

    Level 5:Hash and salt, using express-session and passport module

--- web_devlopement_new/1_NODE/13_gitHub_Secrets/app.js
+++ web_devlopement_new/1_NODE/13_gitHub_Secrets/app.js
@@ -3,11 +3,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 // const encrypt = require("mongoose-encryption");
-//for hashing
 // const md5 = require("md5"); // for hashing
-const bcryptjs = require("bcryptjs");
-const saltRounds = 15;
+// const bcryptjs = require("bcryptjs");
+// const saltRounds = 15;
 
 const app = express();
 
@@ -15,9 +17,20 @@ const app = express();
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(session({
+  secret : "Our little secret",
+  resave : false,
+  saveUninitialized : false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 //connecting to the database
-mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser : true , useUnifiedTopology:true});
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser : true, useUnifiedTopology:true});
+//for removing deprication error
+mongoose.set("useCreateIndex", true);
 
 //creating a schema
 const userSchema = new mongoose.Schema({
@@ -25,11 +38,20 @@ const userSchema = new mongoose.Schema({
   password : String
 });
 
+
+userSchema.plugin(passportLocalMongoose);
+
 //encrypting our key
 // userSchema.plugin( encrypt, { secret: process.env.SECRET , encryptedFields : ["password"] } );
 
 //creating a model
 const User = mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+//after the model creation
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 //handling get request
 app.get("/", function(req,res){
@@ -44,43 +66,51 @@ app.get("/register", function(req,res){
   res.render("register");
 });
 
+app.get("/secrets", function(req,res){
+    if(req.isAuthenticated()){
+      res.render("secrets");
+    } else {
+       res.redirect("/login");
+    }
+});
+
+app.get("/logout", function(req,res){
+   //deauthneticating our userDB
+   req.logout();
+   res.redirect("/");
+});
+
 //handling all the post request
 app.post("/register", function(req,res){
 
-  bcryptjs.hash(req.body.password, saltRounds, function(err,hash){
-
-    const newUser = new User({
-      email :req.body.username,
-      password : hash
+    User.register( {username : req.body.username}, req.body.password, function(err,user){
+      if(err){
+        console.log(err);
+        res.render("/register");
+      } else {
+        passport.authenticate("local")(req, res, function(){
+           res.redirect("/secrets");
+        });
+      }
     });
-
-    newUser.save(function(err){
-       if(err){
-         console.log(err);
-       } else {
-         res.render("secrets");
-       }
-    });
-  });
 });
 
 app.post("/login", function(req,res){
 
-    const username = req.body.username;
-    const password = req.body.password;
+    const user = new User({
+      username : req.body.username,
+      password : req.body.password
+    });
 
-    User.findOne( {email : username}, function(err,foundUser){
-       if(err){
-         console.log(err);
-       } else {
-          if(foundUser) {
-            bcryptjs.compare(password, foundUser.password, function(err,result){
-              if(result === true)
-              res.render("secrets");
-      });
-     }
-    }
-  });
+    req.login(user, function(err){
+      if(err){
+        console.log(err);
+      } else {
+         passport.authenticate("local")(req, res, function(){
+            res.redirect("/secrets");
+         });
+      }
+    });
 });
 
 
